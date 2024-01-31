e494d4ece2acbe460b7d4e2cee0df832142eb9bf
dmfgold <51682153+dmfgold@users.noreply.github.com>
2024-01-31 17:22:21 -0500

    Added MAXSwerve Java Template
    https://github.com/REVrobotics/MAXSwerve-Java-Template
    
    #TODO:
    - Robot vs field relative movement?
    - Connect Autos

--- FRC1414-2024-Code/src/main/java/frc/robot/commands/Autos.java
+++ FRC1414-2024-Code/src/main/java/frc/robot/commands/Autos.java
@@ -4,12 +4,13 @@
 
 package frc.robot.commands;
 
+/*
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos {
-  /** Example static factory for an autonomous command. */
+  // Example static factory for an autonomous command.
   public static Command exampleAuto(ExampleSubsystem subsystem) {
     return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
   }
@@ -18,3 +19,4 @@ public final class Autos {
     throw new UnsupportedOperationException("This is a utility class!");
   }
 }
+*/
