1159c45403ce72febc9ec3b7bfd6ad3d657f3d00
lizthebiz2218 <113944920+lizthebiz2218@users.noreply.github.com>
2024-02-01 18:55:37 -0500

    Update Constants.java
    Use data from 6995's vision constants for the field layout because too lazy to find them myselve

--- FRC1414-2024-Code/src/main/java/frc/robot/Constants.java
+++ FRC1414-2024-Code/src/main/java/frc/robot/Constants.java
@@ -3,6 +3,15 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import java.util.List;
+
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -35,5 +44,45 @@ public final class Constants {
     public static final double kFeedLauncherEffort = 0;
   }
 
-  public
+  public static class VisionConstants {
+    public static final String CAM1_NAME = "OV9281-1";
+    public static final String CAM2_NAME = "OV9281-2";
+    public static final String CAM3_NAME = "OV9281-3";
+    public static final String CAM4_NAME = "OV9281-4";
+
+    private static final double CAM_HEIGHT = Units.inchesToMeters(16);
+    private static final double CAM_X = Units.inchesToMeters(6.6 / 2.0);
+    private static final double CAM_Y = Units.inchesToMeters(15.3/2.0);
+    private static final double CAM_PITCH = Units.degreesToRadians(-15);
+    private static final double CAM_YAW = Units.degreesToRadians(32);
+
+    public static final Transform3d robotToCam1 = new Transform3d(
+      new Translation3d(CAM_X, CAM_Y, CAM_HEIGHT), new Rotation3d(0, CAM_PITCH, CAM_YAW));
+    public static final Transform3d robotToCam2 = new Transform3d(
+      new Translation3d(CAM_X, -CAM_Y- Units.inchesToMeters(0.5), CAM_HEIGHT), 
+      new Rotation3d(0, CAM_PITCH, -CAM_YAW));
+    public static final Transform3d robotToCam3 = new Transform3d(new Translation3d(-CAM_X, CAM_Y, CAM_HEIGHT), 
+      new Rotation3d(0, CAM_PITCH, (Math.PI) - CAM_YAW));
+    public static final Transform3d robotToCam4 = new Transform3d(
+      new Translation3d(-CAM_X, -CAM_Y, CAM_HEIGHT), 
+      new Rotation3d(0, CAM_PITCH, (Math.PI) + CAM_YAW + Units.degreesToRadians(5.5)));
+    
+    public static AprilTagFieldLayout TAG_FIELD_LAYOUT = 
+    new AprilTagFieldLayout(
+      List.of(
+        // Red Alliance Community (Right to Left)
+        new AprilTag(1, new Pose3d(15.513558, 1.071626, 0.462788, new Rotation3d(0, 0, Math.PI))),
+        new AprilTag(2, new Pose3d(15.513558, 2.748026, 0.462788, new Rotation3d(0, 0, Math.PI))),
+        new AprilTag(3, new Pose3d(15.513558, 4.424426, 0.462788, new Rotation3d(0, 0, Math.PI))),
+        // Blue Alliance Double Substation
+        new AprilTag(4, new Pose3d(16.178784, 6.749796, 0.695452, new Rotation3d(0, 0, Math.PI))),
+        // Red Alliance Double Substation
+        new AprilTag(5, new Pose3d(0.36195, 6.749796, 0.695452, new Rotation3d(0, 0, 0))),
+        // Blue Alliance Community (Right to Left)
+        new AprilTag(6, new Pose3d(1.02743, 4.424426, 0.462788, new Rotation3d(0, 0, 0))),
+        new AprilTag(7, new Pose3d(1.02743, 2.748026, 0.462788, new Rotation3d(0, 0, 0))),
+        new AprilTag(8, new Pose3d(1.02743, 1.071626, 0.462788, new Rotation3d(0, 0, 0)))),
+        16.54175,
+        8.0137);
+  }
 }
