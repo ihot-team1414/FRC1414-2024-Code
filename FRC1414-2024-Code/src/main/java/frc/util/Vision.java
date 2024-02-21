0a67964507d4d52ae9c69ed5a91ceb31478a847b
RehanSimpsonAIS <rehan.simpson@aischool.org>
2024-02-21 18:51:33 -0500

    Started working on adding the array list

--- FRC1414-2024-Code/src/main/java/frc/util/Vision.java
+++ FRC1414-2024-Code/src/main/java/frc/util/Vision.java
@@ -1,28 +1,63 @@
 package frc.util;
-
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import java.util.ArrayList;
 
 public class Vision{
+
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+
+    //Array list
     
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-    NetworkTableEntry ta = table.getEntry("ta");
-
-    
-    //read values periodically
-    double x = tx.getDouble(0.0);
-    double y = ty.getDouble(0.0);
-    double area = ta.getDouble(0.0);
-
-    //post to smart dashboard periodically
-    SmartDashboard.putNumber("LimelightX", x);
-    SmartDashboard.putNumber("LimelightY", y);
-    SmartDashboard.putNumber("LimelightArea", area);
-
     
 
+    int numEntries = 5;   
+
+
+    double[] txEntries = new double[5];
+    double[] tyEntries = new double[5];
+    double[] taEntries = new double[5];
+
+    public void Periodic(){
+        NetworkTableEntry tx = table.getEntry("tx");
+        NetworkTableEntry ty = table.getEntry("ty");
+        NetworkTableEntry ta = table.getEntry("ta");
+ 
+        //read values periodically
+        double x = tx.getDouble(0.0);
+        double y = ty.getDouble(0.0);
+        double area = ta.getDouble(0.0);
+
+        //add values to entries arrays
+        for (int i = 0; i < numEntries-2; i++){
+            txEntries[i+1] = txEntries[i];
+        }
+        txEntries[0] = x;
+
+        for (int i = 0; i < numEntries-2; i++){
+            tyEntries[i+1] = tyEntries[i];
+        }
+        tyEntries[0] = y;
+
+        for (int i = 0; i < numEntries-2; i++){
+            taEntries[i+1] = taEntries[i];
+        }
+        taEntries[0] = area;
+
+
+
+        //post to smart dashboard periodically
+        /*
+        SmartDashboard.putNumber("LimelightX", x);
+        SmartDashboard.putNumber("LimelightY", y);
+        SmartDashboard.putNumber("LimelightArea", area);
+        */
+    }
 }
+
+//timestamp to throw awaya older entries, with new entries find the weighted average
+//We need x, y, time, confidence
